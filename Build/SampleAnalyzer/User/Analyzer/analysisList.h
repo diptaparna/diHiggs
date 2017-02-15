2e837e00ebba4778d1dd3191c947c6f6767f0933
Diptaparna Biswas <diptaparna@gmail.com>
2017-02-15 11:56:33 +0100

    First commit

---
+++ Build/SampleAnalyzer/User/Analyzer/analysisList.h
@@ -0,0 +1,12 @@
+#include "SampleAnalyzer/User/Analyzer/basic_plots.h"
+#include "SampleAnalyzer/Process/Analyzer/AnalyzerManager.h"
+#include "SampleAnalyzer/Commons/Service/LogStream.h"
+
+// -----------------------------------------------------------------------------
+// BuildTable
+// -----------------------------------------------------------------------------
+void BuildUserTable(MA5::AnalyzerManager& manager)
+{
+  using namespace MA5;
+  manager.Add("basic_plots",new basic_plots);
+}
